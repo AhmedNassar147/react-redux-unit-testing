a25cc625be98af85308f87c1b175a0317e5852f7
ahmed <ahmed@cyber-crafts.com>
2019-10-05 16:03:51 +0200

    initialize redux and redux-saga

---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import createSagaMiddleware from "redux-saga";
+import reducers from "./injectors/reducers";
+// eslint-disable-next-line
+import rootSagas from "./injectors/sagas";
+const sagaMiddleware = createSagaMiddleware();
+
+const composeEnhancers =
+  process.env.NODE_ENV === "development" &&
+  typeof window === "object" &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(sagaMiddleware));
+
+const store = createStore(reducers, {}, enhancer);
+
+// sagaMiddleware.run(rootSagas);
+
+export default store;
